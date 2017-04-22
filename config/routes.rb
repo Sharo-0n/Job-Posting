cfd02ef5b03d8ecb945a976a99400a063ae0c4d0
Sharo-0n <sharoon@ucsd.edu>
2017-04-22 15:58:00 -0700

    Updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'drinks#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :jobs
 end
